4825dd1a21a1f64a01390dd8dd57bca74d91daa6
agg-real <agg603450@gmail.com>
2025-05-04 01:46:04 +0300

    Update README.lua

--- README.lua
+++ README.lua
@@ -59,12 +59,12 @@ if parts:IsA("BasePart") then
 for i, v in pairs(parts:GetDescendants()) do
     if v:IsA("ProximityPrompt") then 
 local baseCFrame = v.Parent.CFrame
-local offset = CFrame.new(0, 4.255, 0)
+local offset = CFrame.new(0, -4.255, 0)
 local teleportCFrame = baseCFrame * offset 
-        rootPart.CFrame = teleportCFrame * CFrame.Angles(0, math.rad(90), 0)
+        rootPart.CFrame = teleportCFrame
         fireproximityprompt(v)
 rootPart.Anchored = true
-task.wait(0.1)
+task.wait(0.35)
     end 
 end
 end
