dc5d245bafce85904377b25efc0b0f7437643abb
agg-real <agg603450@gmail.com>
2025-05-04 01:08:24 +0300

    Update README.lua

--- README.lua
+++ README.lua
@@ -1,31 +1,80 @@
-local plr = game.Players.LocalPlayer
-local function fireproximityprompt(Obj, Amount, Skip)
-    if Obj.ClassName == "ProximityPrompt" then 
-        Amount = Amount or 1
-        local PromptTime = Obj.HoldDuration
-        if Skip then 
-            Obj.HoldDuration = 0
-        end
-        for i = 1, Amount do 
-            Obj:InputHoldBegin()
-            if not Skip then 
-                wait(Obj.HoldDuration)
-            end
-            Obj:InputHoldEnd()
-        end
-        Obj.HoldDuration = PromptTime
-    else 
-        error("userdata<ProximityPrompt> expected")
+
+local xyi = true
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/ionlyusegithubformcmods/1-Line-Scripts/main/Mobile%20Friendly%20Orion')))()
+local UserInputService = game:GetService("UserInputService")
+local TweenService = game:GetService("TweenService")
+local RunService = game:GetService("RunService")
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local Mouse = LocalPlayer:GetMouse()
+local HttpService = game:GetService("HttpService")
+
+local Window = OrionLib:MakeWindow({Name = "Heaven hub", HidePremium = false, SaveConfig = true})
+local Tab = Window:MakeTab({Name = "Главная", Icon = "rbxassetid://90323304631053"})
+local ex = false
+local co = false
+local used = true
+local sexxx = false
+local first = true
+Tab:AddButton({
+	Name = "Работает только для Heaven Stands",
+	Callback = function()
+  	end    
+})
+
+Tab:AddButton({
+	Name = "Включить Infinite Yield",
+	Callback = function()
+OrionLib:MakeNotification({
+	Name = "Реквариваем Infinite Yield",
+	Content = "Working!",
+	Image = "rbxassetid://18579698048",
+	Time = 5
+})
+      		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+  	end    
+})
+local ez =Tab:AddToggle({
+    Name = "Фармилка боксиков",
+    Default = ex,
+    Callback = function(value)
+        ex = value
     end
-end
+})
+task.spawn(function()
+task.wait(1)
+OrionLib:MakeNotification({
+	Name = "Made by gugugagas team",
+	Content = "Working!",
+	Image = "rbxassetid://18641615063",
+	Time = 5
+})
+	end)
+
+task.spawn(function()
+    while task.wait() do
+if ex then
+rootPart.Anchored = true
 for i, parts in pairs(game:GetService("Workspace").Map.spawnPoints.Box:GetChildren()) do
 if parts:IsA("BasePart") then
 for i, v in pairs(parts:GetDescendants()) do
-
     if v:IsA("ProximityPrompt") then 
-    game.Players.LocalPlayer.Character.Torso.CFrame = v.Parent.CFrame
-        fireproximityprompt(v,2,false)
+local baseCFrame = v.Parent.CFrame
+local offset = CFrame.new(0, -6, 0)
+local teleportCFrame = baseCFrame * offset 
+    local rootPart = player.Character:FindFirstChild("HumanoidRootPart")
+        rootPart.CFrame = teleportCFrame
+task.wait(0.1)
+        fireproximityprompt(v)
+task.wait(0.1)
     end 
 end
 end
 end
+            elseif ex == false then
+   local rootPart = player.Character:FindFirstChild("HumanoidRootPart")
+rootPart.Anchored = false
+            end
+		end
+end)
+
+OrionLib:Init()
